36c5d240e7915a141ccf8cd355851e36d311360b
Elvis Otieno <67471449+otienoelvis@users.noreply.github.com>
2022-06-20 11:44:59 +0300

    removed private keys

--- daraja/config.py
+++ daraja/config.py
@@ -1,9 +1,9 @@
-consumer_key = "H0JqRdy0oWqnJY60gGosulCGIqLfJjbd"
-consumer_secret = "LiNqvVvLQ7ZBTdDk"
-shortCode = "603021"  # c2b
-BusinessShortCode = "174379"  # lipaNaMpesa
-test_msisdn = "254708374149"
+consumer_key = ""
+consumer_secret = ""
+shortCode = ""  # c2b
+BusinessShortCode = ""  # lipaNaMpesa
+test_msisdn = ""
 
-base_url = "https://6fd4-105-160-35-157.ngrok.io"
+base_url = ""
 
 
